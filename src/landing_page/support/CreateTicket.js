acc6d219276c7adb0acca535d0fae4efaf11311f
2101031000177/Tapan <2101031000177@silveroakuni.ac.in>
2025-12-01 14:24:50 +0530

    website design complete

--- src/landing_page/support/CreateTicket.js
+++ src/landing_page/support/CreateTicket.js
@@ -1,6 +1,144 @@
-import React from 'react'
+import React from "react";
 function CreateTicket() {
-    return ( <h1>Create Ticket</h1> );
+  return (
+    <div className="container">
+      <div className="row p-5 mt-5 mb-5">
+        <h2 className="fs-2 ">To create a topic select relevant topic</h2>
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4 className="fs-2 text-center">Account Opening</h4>
+          <a href="" style={{ textDecoration: "None", lineHeight: "2.5" }}>
+            Resident individual
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "None", lineHeight: "2.5" }}>
+            Minor
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "None", lineHeight: "2.5" }}>
+            Non Resident Indian (NRI)
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "None", lineHeight: "2.5" }}>
+            Company, Partnership, HUF and LLP
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "None", lineHeight: "2.5" }}>
+            Glossary
+          </a>
+        </div>
+        <div className="col-4 p-5 mt-2 mb-5">
+          <h4 className="fs-2 text-center">Account Opening</h4>
+          <a href="" style={{ textDecoration: "None", lineHeight: "2.5" }}>
+            Resident individual
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "None", lineHeight: "2.5" }}>
+            Minor
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "None", lineHeight: "2.5" }}>
+            Non Resident Indian (NRI)
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "None", lineHeight: "2.5" }}>
+            Company, Partnership, HUF and LLP
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "None", lineHeight: "2.5" }}>
+            Glossary
+          </a>
+        </div>
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4 className="fs-2 text-center">Account Opening</h4>
+          <a href="" style={{ textDecoration: "None", lineHeight: "2.5" }}>
+            Resident individual
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "None", lineHeight: "2.5" }}>
+            Minor
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "None", lineHeight: "2.5" }}>
+            Non Resident Indian (NRI)
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "None", lineHeight: "2.5" }}>
+            Company, Partnership, HUF and LLP
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "None", lineHeight: "2.5" }}>
+            Glossary
+          </a>
+        </div>
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4 className="fs-2 text-center">Account Opening</h4>
+          <a href="" style={{ textDecoration: "None", lineHeight: "2.5" }}>
+            Resident individual
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "None", lineHeight: "2.5" }}>
+            Minor
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "None", lineHeight: "2.5" }}>
+            Non Resident Indian (NRI)
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "None", lineHeight: "2.5" }}>
+            Company, Partnership, HUF and LLP
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "None", lineHeight: "2.5" }}>
+            Glossary
+          </a>
+        </div>
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4 className="fs-2 text-center">Account Opening</h4>
+          <a href="" style={{ textDecoration: "None", lineHeight: "2.5" }}>
+            Resident individual
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "None", lineHeight: "2.5" }}>
+            Minor
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "None", lineHeight: "2.5" }}>
+            Non Resident Indian (NRI)
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "None", lineHeight: "2.5" }}>
+            Company, Partnership, HUF and LLP
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "None", lineHeight: "2.5" }}>
+            Glossary
+          </a>
+        </div>
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4 className="fs-2 text-center">Account Opening</h4>
+          <a href="" style={{ textDecoration: "None", lineHeight: "2.5" }}>
+            Resident individual
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "None", lineHeight: "2.5" }}>
+            Minor
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "None", lineHeight: "2.5" }}>
+            Non Resident Indian (NRI)
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "None", lineHeight: "2.5" }}>
+            Company, Partnership, HUF and LLP
+          </a>
+          <br />
+          <a href="" style={{ textDecoration: "None", lineHeight: "2.5" }}>
+            Glossary
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CreateTicket;
--- src/landing_page/support/Hero.js
+++ src/landing_page/support/Hero.js
@@ -1,6 +1,31 @@
 import React from 'react'
 function Hero() {
-    return ( <h1>Hero</h1> );
+    return ( 
+    <section className="container-fluid" id='supportHero'>
+      <div className="p-5 " id='supportWrapper'>
+        <h5>Support Portal</h5>
+        <a href=''>Track Tickets</a>
+      </div>
+       <div className=" row p-5 m-3" >
+        <div className='col-6 p-5'>
+            <h2>Search for an answer or browse help topics</h2>
+            <input placeholder='example'></input>
+            <br/>
+            <a href=''>Track Account opening</a>
+            <a href=''> Track segment Activation</a>
+            <a href=''> Intraday Margins</a>
+            <a href=''> Kite User Manual</a>
+        </div>
+        <div className='col-6 p-5'>
+            <h2>Featured</h2>
+            <ol>
+                <li><a href=''>current TakeOvers</a></li>
+                <li><a href=''>latest Intraday levrages</a></li>
+            </ol>
+        </div>
+      </div>
+    </section>
+);
 }
 
 export default Hero;
